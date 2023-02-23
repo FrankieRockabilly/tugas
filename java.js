d81960c2cd983265a6fb59936749f73e6d823e2f
FrankieRockabilly <frankierockabilly@gmail.com>
2023-02-23 15:00:17 +0700

    menambahkan tugas

---
+++ java.js
@@ -0,0 +1,6 @@
+const harga = document.querySelector('#harga');
+const diskon =document.querySelector('#diskon');
+
+harga.addEventListener('click', function(){
+    diskon.innerHTML = "harga awal"
+});
